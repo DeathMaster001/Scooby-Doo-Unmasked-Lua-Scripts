d5d546e57bec61543ce31618355de9dcd0945b41
DeathMaster001 <63770649+DeathMaster001@users.noreply.github.com>
2022-05-09 14:06:56 -0400

    changed scope of functions

--- sdu/memory.lua
+++ sdu/memory.lua
@@ -18,35 +18,27 @@ memory.FoodLookups = FoodLookups
 	
 --Scooby's Health's
 
-local function getScoobyHealth()
+function getScoobyHealth()
 	return ReadValue32(0x559900)
 end
-memory.getScoobyHealth = getScoobyHealth
 
 --Scooby's Mubber
 
-local function getScoobyMubber()
+function getScoobyMubber()
 	return ReadValue32(0x559908)
 end
-memory.getScoobyMubber = getScoobyMubber
 
 --Get current level
 
-local function getLevel()
+function getLevel()
 	return ReadValue8(0x5599F1)
 end
-memory.getLevel = getLevel
-
 --Food lookup to be fixed
 
-local function getFoodId()
+function getFoodId()
 	return ReadValue8(0x55999C)
 end
-memory.getFoodId = getFoodId
 
-local function getFoodName()
+function getFoodName()
 	return ReadValue8(0x55999C)
 end
-memory.getFoodName = getFoodName
-
-return memory
