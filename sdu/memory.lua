44b3aff40b4a1ad308d2a4c5fddbc167b76ba4cc
DeathMaster001 <austinross173@gmail.com>
2022-08-24 19:36:55 -0400

    Implemented more functions for stats.

--- sdu/memory.lua
+++ sdu/memory.lua
@@ -1,13 +1,29 @@
---Scooby's Health's
+--Scooby's Max Health
+
+function getScoobyMaxHealth()
+	return ReadValue32(0x5598FC)
+end
+
+--Scooby's Current Health
 
 function getScoobyHealth()
 	return ReadValue32(0x559900)
 end
 
---Scooby's Mubber
+--Scooby's Snack Count
+
+function getScoobySnack()
+	return ReadValue32(0x559904)
+end
+
+--Scooby's' Mubber Count
 
 function getScoobyMubber()
 	return ReadValue32(0x559908)
+end
+
+function getScoobyClueCount()
+	return ReadValue32(0x55990C)
 end
 
 --Get current level
@@ -15,6 +31,7 @@ end
 function getLevel()
 	return ReadValue8(0x5599F1)
 end
+
 --Food lookup to be fixed
 
 function getFoodId()
